5a840a10e3bb4c6f61f155cae75ebc19ec50a508
Hery <heri@41studio.com>
2014-05-22 09:34:40 +0700

    separate social keys for dev & production

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,10 @@ module Mountseek
     # config.i18n.default_locale = :de
     config.serve_static_assets = true
     config.assets.initialize_on_precompile = false
+    social_keys = File.join(Rails.root, 'config', 'social_keys.yml')
+    CONFIG = HashWithIndifferentAccess.new(YAML::load(IO.read(social_keys)))[Rails.env]
+    CONFIG.each do |k,v|
+      ENV[k.upcase] ||= v
+    end
   end
 end
