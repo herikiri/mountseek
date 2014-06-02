230ad3fd39c5114fdaa27baa49decf661568371e
Mada Aryakusumah <mada@41studio.com>
2014-06-02 11:18:20 +0700

    add gem and TODO

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -4,6 +4,8 @@ class Identity < ActiveRecord::Base
   validates_presence_of :uid, :provider
   validates_uniqueness_of :uid, :scope => :provider
 
+
+  ## TODO : refactor this code, take a look -> http://guides.rubyonrails.org/active_record_querying.html#find-or-create-by
   def self.find_for_oauth(auth)
     identity = find_by(provider: auth.provider, uid: auth.uid)
     identity = create(uid: auth.uid, provider: auth.provider) if identity.nil?
