b39707e2fb53cca12eff72fe820c2c0eb57f47b7
Hery <heri@41studio.com>
2014-06-02 13:08:47 +0700

    refactor code in identity.rb

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -7,8 +7,6 @@ class Identity < ActiveRecord::Base
 
   ## TODO : refactor this code, take a look -> http://guides.rubyonrails.org/active_record_querying.html#find-or-create-by
   def self.find_for_oauth(auth)
-    identity = find_by(provider: auth.provider, uid: auth.uid)
-    identity = create(uid: auth.uid, provider: auth.provider) if identity.nil?
-    identity
+  	identity = find_or_create_by(provider: auth.provider, uid: auth.uid)
   end
 end
