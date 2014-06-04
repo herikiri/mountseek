d71ce671b84acb83635b482d73d35f8a5dd65c4a
Hery <heri@41studio.com>
2014-06-04 17:01:30 +0700

    change backend advertisement proccess flow

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -5,10 +5,7 @@ class AdsController < ApplicationController
   before_action :set_ad, only: [:preview, :publish, :edit_horse]
   before_action :set_fav_ad, only: [:like, :dislike]
 
-  def horse
-    @ad = @package.horses.new
-  end
-
+ 
   def pricing
     @horsePackages = Package.where(type_id: 1)
     @studPackages = Package.where(type_id: 2)
@@ -18,30 +15,8 @@ class AdsController < ApplicationController
     @servicePackages = Package.where(type_id: 6)
   end
 
-  def preview
-    @ad_banner = Picture.find(Ad.last.picture_id)
-  end
-
-  def publish
-    unless Ad.last.published?
-      Ad.last.publish!
-    end
-    respond_to do |format|
-      if Ad.last.published?
-        format.html { redirect_to @ad, notice: 'Ad was successfully published.' }
-      else 
-        format.html { render action: 'preview' }
-      end
-    end
-
-  end
- 
-  def edit_horse
-  end
-
 
   def like
-    
     respond_to do |format|
       if @ad.liked_by @user
         format.html {redirect_to root_url}
