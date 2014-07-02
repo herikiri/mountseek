ccc37b9892db7760badc426110f8dd820211acb0
Mada Aryakusumah <mada@41studio.com>
2014-07-02 10:48:17 +0700

    fix migration error

--- db/migrate/20140616151308_create_disciplines.rb
+++ db/migrate/20140616151308_create_disciplines.rb
@@ -1,7 +1,5 @@
 class CreateDisciplines < ActiveRecord::Migration
   def change
-  	drop_table :disciplines
-  	
     create_table :disciplines do |t|
     	t.string :name
     	t.string :experience
